bf611455c09a8231422a2313ce00736a19cb460a
Lucas Schmidt <lucas_schmidt92@hotmail.com>
2026-03-10 09:19:54 -0300

    Update battleRPG.js

--- DevLingo-Arena/games/battleRPG.js
+++ DevLingo-Arena/games/battleRPG.js
@@ -1,26 +1,80 @@
-const readline = require('readline-sync');
-const gameState = require('../gameState');
+const rl = require("readline-sync");
+const func = require("../gameState");
 
-function battleRPG(){
-    let enemyHP = 30;
-    console.log("\nA Grammar Monster appears!");
+function battleRPG() {
+  const questions = [
+const questions = [
+  {
+    question: "Complete: I ____ a pharmacist (am / is / are)",
+    answer: "am",
+  },
+  {
+    question: "Complete: She ____ my friend (am / is / are)",
+    answer: "is",
+  },
+  {
+    question: "Past of 'work'",
+    answer: "worked",
+  },
+  {
+    question: "Past of 'study'",
+    answer: "studied",
+  },
+  {
+    question: "Complete: I ____ coffee every morning (drink / drinks)",
+    answer: "drink",
+  },
+  {
+    question: "Complete: He ____ Pokémon (like / likes)",
+    answer: "likes",
+  },
+  {
+    question: "Plural of 'dog'",
+    answer: "dogs",
+  },
+  {
+    question: "Male pronoun",
+    answer: "he",
+  }
+];
+  ];
+  let loop = true;
+  let indx_Loop = 0;
+  let enemyHP = 30;
+  let questions_Loop = [];
 
-    while(enemyHP > 0) {
-
-        console.log("\nEnemy HP:", enemyHP);
-        console.log("Answer correctly to attack.");
-
-        let answer = readline.question("Past of 'go': ")
-
-        if (answer.toLowerCase() === "went"){
-            console.log('You sucesfully hit the enemy!')
-            enemyHP = 10;
-        } else {
-            console.log("You miss! The monster hits you!");
-            gameState.loseLife();
-        }
+  for (let i = 0; i < 3; i++) {
+    while (loop) {
+      let indx = Math.floor(Math.random() * questions.length);
+      if (questions_Loop.length > 0) {
+        questions_Loop.forEach((quest) => {
+          if (questions[indx - 1].answer !== quest.answer) {
+            loop = false;
+          } else {
+            loop = true;
+          }
+        });
+      }
     }
+    questions_Loop.push(questions[indx - 1]);
+  }
 
+  console.log("An Enemy appears!");
+  while (enemyHP > 0) {
+    console.log("Monster's HP: " + enemyHP);
+    console.log("Answer correctly to attack!");
+
+    let answer = rl.question(`${questions_Loop[indx_Loop].question}: `);
+
+    if (answer === questions_Loop[indx_Loop].answer) {
+      console.log("Correct!");
+      func.addPoints(10);
+    } else {
+      console.log("Wrong! Correct answer: " + questions_Loop[indx_Loop].answer);
+      func.loseLife();
+    }
+    indx_Loop++;
+  }
 }
 
-module.exports = battleRPG
+module.exports = battleRPG;
