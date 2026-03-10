7c679835471bb6b5bf54720d98ca08f8cb1278ce
Lucas Schmidt <lucas_schmidt92@hotmail.com>
2026-03-10 09:02:46 -0300

    Add additional questions to quiz game
    Added new quiz questions to enhance the quiz game.

--- DevLingo-Arena/games/quiz.js
+++ DevLingo-Arena/games/quiz.js
@@ -3,24 +3,41 @@ const gameState = require('../gameState');
 
 function quiz(){
     const questions = [
-        {
-            question: " She ____ my friend.",
-            options: ["1) am", "2) is", "3) are"],
-            answer: "2"
-        },
-        {
-            question: " They ____ students.",
-            options: ["1) is", "2) are", "3) am"],
-            answer: "2"
-        },
+{
+    question: " She ____ not happy today.",
+    options: ["1) is", "2) are", "3) am"],
+    answer: "1"
+},
 
-        {
-            question: " I ___ tired yesterday, because ____ played the soccer",
-            options: ["1) is/they", "2) are/we", "3) was/I"],
-            answer: "3"
-        },
-  
-    ];
+{
+    question: " I ____ not like coffee.",
+    options: ["1) do", "2) does", "3) did"],
+    answer: "1"
+},
+
+{
+    question: " They ____ not at home yesterday.",
+    options: ["1) are", "2) were", "3) is"],
+    answer: "2"
+},
+
+{
+    question: " We ____ soccer last night.",
+    options: ["1) play", "2) played", "3) playing"],
+    answer: "2"
+},
+
+{
+    question: " He ____ to school yesterday.",
+    options: ["1) go", "2) goes", "3) went"],
+    answer: "3"
+},
+
+{
+    question: " She ____ watching TV now.",
+    options: ["1) is", "2) are", "3) am"],
+    answer: "1"
+}
 
     questions.forEach(q => {
 
